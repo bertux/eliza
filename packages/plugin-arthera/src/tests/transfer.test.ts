30d4054aec916e7a49e12511828f14e543286581
Bertrand Juglas <b@juglas.name>
2025-01-03 00:34:39 +0100

    test: enhance transfer tests with additional wallet provider and address validation

--- packages/plugin-arthera/src/tests/transfer.test.ts
+++ packages/plugin-arthera/src/tests/transfer.test.ts
@@ -1,45 +1,65 @@
 import { describe, it, expect, beforeEach } from "vitest";
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { Account, Chain } from "viem";
+import { generatePrivateKey } from "viem/accounts";
+import { Chain } from "viem";
+import { getEnvVariable } from "@elizaos/core";
 
 import { TransferAction } from "../actions/transfer";
 import { WalletProvider } from "../providers/wallet";
 
 describe("Transfer Action", () => {
     let wp: WalletProvider;
+    let wp1: WalletProvider;
 
     beforeEach(async () => {
         const pk = generatePrivateKey();
+        const pk1 = getEnvVariable("EVM_PRIVATE_KEY") as `0x${string}`;
         const customChains = prepareChains();
         wp = new WalletProvider(pk, customChains);
+        wp1 = new WalletProvider(pk1, customChains);
     });
     describe("Constructor", () => {
         it("should initialize with wallet provider", () => {
-            const ta = new TransferAction(wp);
+            const ta1 = new TransferAction(wp1);
 
-            expect(ta).toBeDefined();
+            expect(ta1).toBeDefined();
         });
     });
     describe("Transfer", () => {
         let ta: TransferAction;
-        let receiver: Account;
+        let ta1: TransferAction;
+        let receiverAddress: `0x${string}`;
 
         beforeEach(() => {
             ta = new TransferAction(wp);
-            receiver = privateKeyToAccount(generatePrivateKey());
+            ta1 = new TransferAction(wp1);
+            receiverAddress = wp1.getAddress();
         });
 
         it("throws if not enough gas", async () => {
             await expect(
                 ta.transfer({
                     fromChain: "arthera",
-                    toAddress: receiver.address,
+                    toAddress: receiverAddress,
                     amount: "1",
                 })
             ).rejects.toThrow(
                 "Transfer failed: The total cost (gas * gas fee + value) of executing this transaction exceeds the balance of the account."
             );
         });
+
+        it("transfers tokens", async () => {
+            const tx = await ta1.transfer({
+                fromChain: "arthera",
+                toAddress: receiverAddress,
+                amount: "0.001",
+            });
+
+            expect(tx).toBeDefined();
+            expect(tx.from).toEqual(wp1.getAddress());
+            expect(tx.to).toEqual(receiverAddress);
+            expect(tx.value).toEqual(1000000000000000n);
+        });
+
     });
 });
 
