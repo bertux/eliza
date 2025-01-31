8bc202e6c0c9df564012315759691d3641d14700
Bertrand Juglas <b@juglas.name>
2025-01-31 16:26:06 +0100

    feat(plugin-lukso): enhance transfer tests with optional wallet provider and remove create token tests

--- packages/plugin-lukso/src/tests/transfer.test.ts
+++ packages/plugin-lukso/src/tests/transfer.test.ts
@@ -1,16 +1,22 @@
 import { describe, it, expect, beforeEach } from "vitest";
 import { generatePrivateKey } from "viem/accounts";
 import { Chain } from "viem";
+import { getEnvVariable } from "@elizaos/core";
 import { TransferAction } from "../actions/transfer";
 import { WalletProvider } from "../providers/wallet";
 
 describe("Transfer Action", () => {
     let wp: WalletProvider;
+    let wp1: WalletProvider;
 
     beforeEach(async () => {
         const pk = generatePrivateKey();
+        const pk1 = getEnvVariable("LUKSO_PRIVATE_KEY") as `0x${string}`;
         const customChains = prepareChains();
         wp = new WalletProvider(pk, customChains);
+        if (pk1) {
+            wp1 = new WalletProvider(pk1, customChains);
+        }
     });
     describe("Constructor", () => {
         it("should initialize with wallet provider", () => {
@@ -21,11 +27,18 @@ describe("Transfer Action", () => {
     });
     describe("Transfer", () => {
         let ta: TransferAction;
+        let ta1: TransferAction;
         let receiverAddress: `0x${string}`;
 
         beforeEach(() => {
             ta = new TransferAction(wp);
-            receiverAddress = wp.getAddress();
+            if (wp1) {
+                ta1 = new TransferAction(wp1);
+                receiverAddress = wp1.getAddress();
+            }
+            else {
+                receiverAddress = wp.getAddress();
+            }
         });
 
         it("throws if not enough gas", async () => {
@@ -39,6 +52,22 @@ describe("Transfer Action", () => {
                 "Transfer failed: The total cost (gas * gas fee + value) of executing this transaction exceeds the balance of the account."
             );
         });
+
+        if (wp1) {
+            it("transfers tokens", async () => {
+                const tx = await ta1.transfer({
+                    fromChain: "arthera",
+                    toAddress: receiverAddress,
+                    amount: "0.001",
+                });
+
+                expect(tx).toBeDefined();
+                expect(tx.from).toEqual(wp1.getAddress());
+                expect(tx.to).toEqual(receiverAddress);
+                expect(tx.value).toEqual(1000000000000000n);
+            });
+        }
+
     });
 });
 
